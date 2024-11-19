d324e3e4b9fd53b6aa8fe9e57ce7a6f961998ccf
조하민 <chohamin0611@gmail.com>
2024-11-19 20:26:33 +0900

    "CRUD"

--- src/User.java
+++ src/User.java
@@ -1,4 +1,68 @@
+import java.time.LocalDateTime;
+
 public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String createAt;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createAt = LocalDateTime.now().toString();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long setId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
 
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", createAt='" + createAt + '\'' +
+                '}';
+    }
 }
