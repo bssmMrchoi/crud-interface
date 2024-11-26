a16aedd745fbb5b3a5c7c9aff3ffde650928d88b
pdh0128 <pdh0128a@gmail.com>
2024-11-26 20:13:17 +0900

    푸쉬

--- src/UserTest.java
+++ src/UserTest.java
@@ -1,8 +1,7 @@
 public class UserTest {
     public static void main(String[] args) {
-//        CrudService<User> userService = new UserService();
-//
-//        User user1 = userService.create(new User("choi", "choi@bssm.hs.kr", "q1w2e3"));
-//        User user2 = userService.create(new User("kim", "kim@bssm.hs.kr", "1q2w3e"));
+        CrudService<User> userService = new UserService();
+        User user1 = userService.create(new User("park", "park@bssm.hs.kr", "q1w2e3"));
+        User user2 = userService.create(new User("mandong", "kim@bssm.hs.kr", "1q2w3e"));
     }
 }
