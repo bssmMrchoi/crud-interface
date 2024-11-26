a827a76844456c8a1b84fcb12528326263e2c999
whdkfk <joara080827@gmail.com>
2024-11-26 20:20:16 +0900

    1314 조아라

--- src/UserTest.java
+++ src/UserTest.java
@@ -2,27 +2,22 @@ public class UserTest {
     public static void main(String[] args) {
         CrudService<User> userService = new UserService();
 
-        //C
         User user1 = userService.create(new User("choi", "choi@bssm.hs.kr", "q1w2e3"));
         User user2 = userService.create(new User("kim", "kim@bssm.hs.kr", "1q2w3e"));
         System.out.println(user1);
         System.out.println(user2);
 
-        //R
         System.out.println("\n id = 2인 user Read 할거임");
         User find = userService.read(2L);
         System.out.println(find);
 
-        //U
         User updateUser = userService.update(1L, new User("park", "park@email", "q1w2e3"));
         System.out.println("\n" + updateUser);
 
-        //D
         System.out.println(userService.delete(1L));
         User find1 = userService.read(1L);
         System.out.println(find1);
 
-        // List All -> 전부 다 보여줘
         userService.listAll().forEach(System.out::println);
     }
 }
