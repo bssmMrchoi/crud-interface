f8a09a0c0b9d3937099c6a36d7a374f40922a2d9
chs08 <t86662998@gmail.com>
2024-11-26 20:20:39 +0900

    1316 천현수

--- src/User.java
+++ src/User.java
@@ -1,3 +1,63 @@
-public class User {
+import java.time.LocalDate;
 
+public class User {
+  private Long id;
+  private String name;
+  private String password;
+  private String email;
+  private String createAt;
+
+  public User(String name, String password, String email, String createAt) {
+    this.name = name;
+    this.password = password;
+    this.email = email;
+    this.createAt = LocalDate.now().toString();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getCreateAt() {
+    return createAt;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", password='" + password + '\'' +
+            ", email='" + email + '\'' +
+            ", createAt='" + createAt + '\'' +
+            '}';
+  }
 }
--- src/UserTest.java
+++ src/UserTest.java
@@ -4,5 +4,6 @@ public class UserTest {
 //
 //        User user1 = userService.create(new User("choi", "choi@bssm.hs.kr", "q1w2e3"));
 //        User user2 = userService.create(new User("kim", "kim@bssm.hs.kr", "1q2w3e"));
+
     }
 }
