3bbe21d5f25fc302872cf6aff813b899f090d555
심현진 <hyunjin.dev143@gmail.com>
2024-11-26 20:07:42 +0900

    feat : HashMap으로 유저서비스 구현하기

--- src/User.java
+++ src/User.java
@@ -1,3 +1,49 @@
-public class User {
+import java.time.LocalDate;
 
+public class User {
+  private Long id;
+  private String name;
+  private String password;
+  private String email;
+  private String createAt;;
+  static private Long count = 1L;
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", password='" + password + '\'' +
+            ", email='" + email + '\'' +
+            ", createAt='" + createAt + '\'' +
+            '}';
+  }
+
+  public User(String name, String password, String email) {
+    this.id = count++;
+    this.createAt = LocalDate.now().toString();
+    this.name = name;
+    this.password = password;
+    this.email = email;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getCreateAt() {
+    return createAt;
+  }
 }
